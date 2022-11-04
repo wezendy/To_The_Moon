75d429f311c94301ee0cdc03afe85d132878a200
wezendy <vszabi98@gmail.com>
2022-11-04 19:10:37 +0100

    Initial commit

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Game");
+
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("StartScreen");
+
+    }
+
+    public void LoadOptions()
+    {
+        SceneManager.LoadScene("Options");
+
+    }
+}
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] Image soundOn;
+    [SerializeField] Image soundOff;
+    private bool muted = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    { 
+        if(!PlayerPrefs.HasKey("muted"))
+        {
+            PlayerPrefs.SetInt("muted", 0);
+            Load();
+        }
+
+        else
+        {
+            Load();
+        }
+        
+        UpdateButton();
+        AudioListener.pause = muted;
+    }
+
+    
+    
+    public void OnSoundButtonPress()
+    {
+        if(muted == false)
+        {
+            muted = true;
+            AudioListener.pause =true;
+        }
+
+        else 
+        {
+            muted = false;
+            AudioListener.pause = false;
+        }
+
+        Save();
+        UpdateButton();
+    }
+
+    private void UpdateButton()
+    {
+        if(muted == false)
+        {
+            soundOn.enabled = true;
+            soundOff.enabled = false;
+        }
+
+        else
+        {
+            soundOn.enabled = false;
+            soundOff.enabled = true;
+        }
+    }
+
+    private void Load()
+    {
+        muted = PlayerPrefs.GetInt("muted") == 1;
+    }
+    
+    private void Save()
+    {
+        PlayerPrefs.SetInt("muted", muted ? 1 : 0);
+    }
+}
